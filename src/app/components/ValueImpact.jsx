4289d527cb51d4408128e633d3d7b9b189a9731c
madhuyadav011 <madhu@greensturn.com>
2024-06-18 14:39:34 +0530

    changes

--- src/app/components/ValueImpact.jsx
+++ src/app/components/ValueImpact.jsx
@@ -6,10 +6,10 @@ const ValueImpact = () => {
       <h2 className="section-title text-center">Value & Impact</h2>
    
       <div className="flex row mx-md-n5  justify-content-center">
-        <div className="col-12 col-md-6 col-lg-4 ">
-          <div className='w-100 m-2 card bg-light '>
+        <div className="col-12 col-md-6 col-lg-4 d-flex  ">
+          <div className="w-100 m-2 card bg-light ">
             <div className="icon-wrapper ">
-              <img className=" border border-success rounded-circle p-3" src="/Images/Mask group 1.png" alt="Mask group 2" />
+              <img className="bg-white border border-success rounded-circle p-3" src="/Images/Mask group 1.png" alt="Mask group 2" />
             </div>
             <div className="content ">
               <h3>Understand Sustainable Performance</h3>
@@ -19,10 +19,10 @@ const ValueImpact = () => {
             </div>
           </div> 
         </div>
-        <div className="col-12 col-md-6 col-lg-4 ">
-          <div></div>
+        <div className="col-12 col-md-6 col-lg-4 d-flex  ">
+          <div className="w-100 m-2 card bg-light">
           <div className="icon-wrapper">
-            <img className=" border border-success rounded-circle p-3" src="/Images/Mask group 2.png" alt="Mask group 2" />
+            <img className=" bg-white border border-success rounded-circle p-3" src="/Images/Mask group 2.png" alt="Mask group 2" />
           </div>
           <div className="content">
             <h3>Track Progress and Improve</h3>
@@ -30,16 +30,19 @@ const ValueImpact = () => {
               Custom improvement plans equip you with the tools to achieve your sustainability goals. Set specific targets, track progress on a user-friendly dashboard, and gain valuable insights through E-learning modules and training partners. This comprehensive approach empowers you to build expertise and ensure measurable progress towards a sustainable future.
             </p>
           </div>
+          </div>
         </div>
-        <div className="col-12 col-md-6 col-lg-4 ">
+        <div className="col-12 col-md-6 col-lg-4 d-flex ">
+          <div className="w-100 m-2 card bg-light">
           <div className="icon-wrapper">
-            <img className=" border border-success rounded-circle p-3" src="/Images/Mask group 3.png" alt="Mask group 1" />
+            <img className="bg-white border border-success rounded-circle p-3" src="/Images/Mask group 3.png" alt="Mask group 1" />
           </div>
           <div className="content">
             <h3>Add Value to your Clients</h3>
             <p>
               Streamline your sustainability journey with collaborative tools. Our platform empowers you to prioritize actions, track progress, and seamlessly share your scorecard with multiple customers. This fosters transparency and collaboration within your supply chain, ultimately helping your customers measure and report their own environmental footprint.
             </p>
+          </div>
           </div>
         </div>
       </div>
