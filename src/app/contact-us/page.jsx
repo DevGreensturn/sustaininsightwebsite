de428d97fecfa01caac5854258cae442cd29b18e
greensturn-prachi <prachi@greensturn.com>
2024-06-28 17:53:13 +0530

    solve issue of page

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+
 import CommonBanner from '../components/common/CommonBanner'
 import ContactUsForm from './ContactUsForm'
 import CommonFooterSection from '../components/common/CommonFooterSection'
@@ -7,7 +7,7 @@ import CommonFooterSection from '../components/common/CommonFooterSection'
 export default function page() {
   return (
     <div>
-        <Navbar />
+   
         <CommonBanner rightheading="Contact Us" image="/Images/all-banner/contact-us.png"/>
         <ContactUsForm />
         <CommonFooterSection />
--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import CommonBanner from '../components/common/CommonBanner'
 import NewsBanner from './NewsBanner'
 import NewsTab from './NewsTab'
@@ -7,7 +6,6 @@ import NewsTab from './NewsTab'
 export default function page() {
   return (
     <div>
-      <Navbar />
       <CommonBanner rightheading="Our News" image="/Images/all-banner/Image-news.png"/>
       <NewsBanner />
       <NewsTab />
